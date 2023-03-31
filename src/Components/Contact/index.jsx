76418fd0756f33c0da2feb12ec040debac8e51f2
Akash <adesigns>
2023-03-31 10:00:50 -0400

    removed handleSubmit function for now

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -8,25 +8,27 @@ const TEMPLATE_ID = "template_mhea5zf";
 const USER_ID = "_4kt98Jj-6hp6cWoO";
 
 const Contact = () => {
-    const handleOnSubmit = (e) => {
-        e.preventDefault();
+    // const handleOnSubmit = (e) => {
+    //     e.preventDefault();
 
-        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID).then((result) => {
-            if (result.text === "OK") {
-                document.querySelector(".submit-button").innerHTML = "Message sent. Thank you";
-            } else {
-                document.querySelector(".submit-button").innerHTML = "Sorry. Your message was not sent";
-            }
-        }, (error) => {
-            console.log(error.text);
-        });
+    //     emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID).then((result) => {
+    //         if (result.text === "OK") {
+    //             document.querySelector(".submit-button").innerHTML = "Message sent. Thank you";
+    //         } else {
+    //             document.querySelector(".submit-button").innerHTML = "Sorry. Your message was not sent";
+    //         }
+    //     }, (error) => {
+    //         console.log(error.text);
+    //     });
 
-        e.target.reset();
-    }
+    //     e.target.reset();
+    // }
+
     return (
         <>
             <div className="contact-form-container" style={{display: "none"}}>
-                <Form className="contact-content" onSubmit={(e) => handleOnSubmit(e)}>
+                {/* <Form className="contact-content" onSubmit={(e) => handleOnSubmit(e)}> */}
+                <Form className="contact-content">
                     <h1>Hit us up!</h1>
                     <Form.Field control={Input} name='user_name' placeholder='Nickname or full name *' required />
                     <Form.Field control={Input} name='user_email' placeholder='Email *' required/>
